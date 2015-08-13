43f3475f51c56c75f7fd3afc5bde949b54831cc4
PMC <engineering+bitbucket@pmc.com>
2015-08-13 14:10:16 +0000

    ------------------------------------------------------------------------ r209623 | tarendai | 2015-08-13 14:04:48 +0000 (Thu, 13 Aug 2015) | 1 line Changed paths:    A /pmc-plugins/webdam-asset-chooser    A /pmc-plugins/webdam-asset-chooser/assets    A /pmc-plugins/webdam-asset-chooser/assets/assetchooser-loader.js    A /pmc-plugins/webdam-asset-chooser/assets/assetchooser-loader.noframe.js    A /pmc-plugins/webdam-asset-chooser/assets/setvariable.html    A /pmc-plugins/webdam-asset-chooser/assets/webdam.png    A /pmc-plugins/webdam-asset-chooser/readme.txt    A /pmc-plugins/webdam-asset-chooser/webdam-asset-chooser.php
    VIP: Deploying the webdam-asset-chooser for PMC as per #42828-z
    ------------------------------------------------------------------------

---
+++ assets/assetchooser-loader.noframe.js
@@ -0,0 +1,62 @@
+(function() {
+	tinymce.create('tinymce.plugins.WebDAMAssetPicker', {
+		init: function(ed, mainUrl) {
+			ed.addButton('btnWebDAMAssetPicker',
+			{
+				title: 'Asset Picker',
+				image: mainUrl + '/webdam.png',
+				cmd: 'showAssetPicker',
+				classes: 'widget btn btnWebDAMAssetPicker',
+				onclick: function() { }
+			});
+
+			ed.addCommand('showAssetPicker', function() {
+				var params = [{label:"Embed the link", action:"getAssetId", showAddLink:"true", sessionMode:"session"}];
+				var returnPath = mainUrl + '/setvariable.html';
+				window.open(asset_picker_domain + '/assetpicker/assetpicker.plugin.php?returnUrl=' + encodeURIComponent(returnPath) +
+						'&params=' + encodeURIComponent(JSON.stringify(params)));
+
+				// also initiate the method that checks cookie and inserts the image when set
+				window.setInterval(function() {
+					var webDAMHTMLPath = asset_picker_domain;
+					var re = new RegExp("widgetEmbedValue=([^;]+)");
+					var value = re.exec(document.cookie);
+					var currentCookieValue = (value != null) ? unescape(value[1]) : null;
+
+					if (currentCookieValue != '' && currentCookieValue != null) {
+						// clear the cookie value
+						document.cookie = "widgetEmbedValue=;path=/;";
+						var returnedImage = JSON.parse(currentCookieValue);
+						if (returnedImage.embedType != 'dismiss') {
+							if (returnedImage.embedType == 'preview') {
+								var elem_img = jQuery( '<img>' ).attr( 'src', returnedImage.url ).attr( 'alt', returnedImage.filename );
+
+								ed.execCommand( 'mceInsertContent', 0, elem_img.prop( 'outerHTML' ) );
+							} else {
+								var textLink = prompt('Please enter the label of your link', returnedImage.filename);
+
+								var elem_anchor = jQuery( '<a></a>' ).attr( 'href', webDAMHTMLPath + '/download.php?id=' + returnedImage.id ).text( textLink );
+
+								ed.execCommand( 'mceInsertContent', 0, elem_anchor.prop( 'outerHTML' ) );
+							}
+						}
+
+						currentCookieValue = null;
+					}
+				}, 500);
+			});
+		},
+
+		getInfo: function() {
+			return {
+				longname: "WebDAM Asset Picker",
+				author: 'WebDAM',
+				authorurl: 'http://webdam.com',
+				infourl: 'http://webdam.com',
+				version: "1.0"
+			};
+		}
+	});
+	// Register plugin
+	tinymce.PluginManager.add('webdam_asset_picker', tinymce.plugins.WebDAMAssetPicker);
+})();
---
+++ readme.txt
@@ -0,0 +1,34 @@
+=== Advanced Custom Fields: Gallery Field ===
+Author: WebDAM
+Author URI: http://www.webdam.com
+Plugin URI: http://www.webdam.com
+Requires at least: 3.5
+Tested up to: 4.1
+Version: 1.0
+
+
+== Copyright ==
+Copyright 2014 - 2015 WebDAM
+
+This software is NOT to be distributed, but can be INCLUDED in WP themes and Plugins: Premium or Contracted.
+
+== Description ==
+
+= Embed your WebDAM assets easily in your own Wordpress application =
+
+The WebDAM Asset Chooser for Wordpress allows you to embed assets from your WebDAM account directly into Wordpress pages or posts. Simply enter the domain that you integrate with in the WebDAM settings and you are all set.
+
+
+== Installation ==
+
+To run this plugin, simply install the scripts and then:
+1. Go to the new 'WebDAM' link under admin settings (see screenshot 2)
+2. Enter the domain that you wish to link to.
+
+And that's it, you are all set. You can now embed assets by clicking on the new WebDAM icon when using the Wordpress editor.
+
+
+== Changelog ==
+
+= 1.0 =
+* Initial Release.
