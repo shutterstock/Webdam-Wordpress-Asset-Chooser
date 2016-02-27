400cfad881497fa15adb504e002e8f3a84fec491
James Mehorter <james.mehorter@pmc.com>
2016-02-26 20:39:27 -0500

    PMCVIP-831 - A lil' readme love and version bump

--- webdam-asset-chooser.php
+++ webdam-asset-chooser.php
@@ -3,14 +3,14 @@
  Plugin Name: WebDAM Asset Chooser
  Plugin URI: http://webdam.com/
  Description: Import WebDAM assets into WordPress.
- Version: 1.1
+ Version: 1.2.0
  Author: WebDAM, PMC, Amit Gupta, James Mehorter
  Author URI: http://webdam.com/
 */
 
 namespace Webdam;
 
-define( 'WEBDAM_PLUGIN_VERSION', '1.0.0' );
+define( 'WEBDAM_PLUGIN_VERSION', '1.2.0' );
 define( 'WEBDAM_PLUGIN_DIR', __DIR__ );
 define( 'WEBDAM_PLUGIN_URL', plugins_url( '', __FILE__ ) );
 define( 'WEBDAM_PLUGIN_SLUG', 'webdam-asset-chooser' );
