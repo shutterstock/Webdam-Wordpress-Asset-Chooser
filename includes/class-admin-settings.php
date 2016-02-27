4c470204448f8a14ba78ff21b44f62813b456add
James Mehorter <james.mehorter@pmc.com>
2016-02-26 20:31:37 -0500

    PMCVIP-831 - Refine the admin UI experience
    Now the user is guided through the authorization process and provided a display of the authentication status.

--- includes/class-admin-settings.php
+++ includes/class-admin-settings.php
@@ -3,8 +3,7 @@
 namespace Webdam;
 
 /**
- * Class Admin
- * @package Webdam
+ * WebDAM Admin Settings Page (Settings > WebDAM)
  */
 class Admin {
 
@@ -14,12 +13,9 @@ class Admin {
 	private static $_instance;
 
 	/**
-	 * Holds the webdam settings
-	 */
-	private $options;
-
-	/**
 	 * Fetch THE instance of the admin object
+	 *
+	 * @param null
 	 *
 	 * @return Admin object instance
 	 */
@@ -35,7 +31,11 @@ class Admin {
 	}
 
 	/**
-	 * Admin constructor.
+	 * Set WordPress hooks
+	 *
+	 * @param null
+	 *
+	 * @return null
 	 */
 	public function __construct() {
 
@@ -43,16 +43,56 @@ class Admin {
 		add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
 		add_action( 'admin_init', array( $this, 'create_settings_page_elements' ) );
 
+		// Enqueue styles and scripts
+		add_action( 'admin_enqueue_scripts', array( $this, 'wp_enqueue_scripts' ) );
+
 		// Display a notice when credentials are needed
 		if ( ! \webdam_get_settings() ) {
 			add_action( 'admin_notices', array( $this, 'show_admin_notice' ) );
 		}
 	}
-	
+
+	/**
+	 * Enqueue admin scripts and styles
+	 *
+	 * @param null
+	 *
+	 * @return null
+	 */
+	public function wp_enqueue_scripts() {
+
+		// Only enqueue these items on our settings page
+		if ( ! empty( $_GET['page'] ) ) {
+
+			if ( 'webdam-settings' === $_GET['page'] ) {
+
+				// Enqueue the webdam admin JS
+				wp_enqueue_script(
+					'webdam-admin-settings',
+					WEBDAM_PLUGIN_URL . 'assets/admin-settings.js',
+					array( 'jquery' ),
+					false,
+					false
+				);
+
+				// Enqueue the webdam admin CSS
+				wp_enqueue_style(
+					'webdam-admin-settings',
+					WEBDAM_PLUGIN_URL . 'assets/admin-settings.css',
+					array(),
+					false,
+					'screen'
+				);
+			}
+		}
+	}
+
 	/**
 	 * Show a notice to admin users to update plugin options
 	 *
-	 * @return void
+	 * @param null
+	 *
+	 * @return null
 	 */
 	public function show_admin_notice() {
 		/*
@@ -74,6 +114,10 @@ class Admin {
 
 	/**
 	 * Create the settings page
+	 *
+	 * @param null
+	 *
+	 * @return null
 	 */
 	public function add_plugin_page() {
 
@@ -87,31 +131,11 @@ class Admin {
 	}
 
 	/**
-	 * Create the settings page contents/form fields
-	 */
-	public function create_settings_page() {
-
-		// Set class property
-		$this->settings = get_option( 'webdam_settings' ); ?>
-
-		<div class="wrap">
-			<h2>WebDAM Settings</h2>
-			<form method="post" action="options.php"><?php
-
-				// This prints out all hidden setting fields
-				settings_fields( 'webdam_settings' );
-
-				do_settings_sections( 'webdam-settings' );
-
-				submit_button(); ?>
-
-			</form>
-		</div><?php
-
-	}
-
-	/**
-	 * Register page settings, sections, and fields
+	 * Register our setting
+	 *
+	 * @param null
+	 *
+	 * @return null
 	 */
 	public function create_settings_page_elements() {
 
@@ -123,147 +147,120 @@ class Admin {
 			'webdam_settings',
 			array( $this, 'webdam_settings_input_sanitization' )
 		);
-
-		/**
-		 * WebDAM Account Section
-		 */
-		add_settings_section(
-			'webdam_settings_section_webdam_account',
-			'WebDAM Account',
-			array( $this, 'webdam_settings_section_webdam_account_info' ),
-			'webdam-settings'
-		);
-
-		add_settings_field(
-			'webdam_account_domain',
-			'Domain',
-			array( $this, 'webdam_account_domain_input_field' ),
-			'webdam-settings',
-			'webdam_settings_section_webdam_account'
-		);
-
-		add_settings_field(
-			'webdam_account_username',
-			'Username',
-			array( $this, 'webdam_account_username_input_field' ),
-			'webdam-settings',
-			'webdam_settings_section_webdam_account'
-		);
-
-		add_settings_field(
-			'webdam_account_password',
-			'Password',
-			array( $this, 'webdam_account_password_input_field' ),
-			'webdam-settings',
-			'webdam_settings_section_webdam_account'
-		);
-
-		/**
-		 * WebDAM API Section
-		 *
-		 * @todo "Enable WebDAM API Integration" checkbox
-		 * "Enable to use features such as sideloading metadata"
-		 *
-		 * enter client id and secret
-		 *
-		 * @todo allow grant type selection in admin default should be auth_code but allow fallback to password.
-		 * @todo only display auth code method once client id and secret at set
-		 * @todo likewise only show username/pass UNLESS we can somehow prepopulate them when the chooser iframe opens
-		 * @todo click auth code link (https://apiv2.webdamdb.com/oauth2/authorize?response_type=code&client_id=b632bb166bf7bdc2370e363d0eb87e70cee3bc2b&redirect_uri=wwd.com&state=STATE) redirect uri should be the settings page, grab auth_code out of the URL and save/fetch token with it
-		 *
-		 */
-		add_settings_section(
-			'webdam_settings_section_webdam_api',
-			'API Settings',
-			array( $this, 'webdam_settings_section_webdam_api_info' ),
-			'webdam-settings'
-		);
-
-		add_settings_field(
-			'api_client_id',
-			'Client ID',
-			array( $this, 'api_client_id_input_field' ),
-			'webdam-settings',
-			'webdam_settings_section_webdam_api'
-		);
-
-		add_settings_field(
-			'api_client_secret',
-			'Client Secret',
-			array( $this, 'api_client_secret_input_field' ),
-			'webdam-settings',
-			'webdam_settings_section_webdam_api'
-		);
 	}
 
 	/**
-	 * Print the WebDAM Account Section text
+	 * Create the settings page contents/form fields
+	 *
+	 * @param null
+	 *
+	 * @return null
 	 */
-	public function webdam_settings_section_webdam_account_info() {
-		esc_html_e( 'Enter your WebDAM Account information below:', 'webdam' );
-	}
+	public function create_settings_page() {
 
-	/**
-	 * Print the API Section text
-	 */
-	public function webdam_settings_section_webdam_api_info() {
-		esc_html_e( 'Enter your API credentials below below:', 'webdam' );
-	}
+		// Set some default items
+		$api_status_text = __( 'API NOT Authenticated', 'webdam' );
+		$api_status_class = 'not-authenticated';
 
-	/**
-	 * Get the settings option array and print one of its values
-	 */
-	public function webdam_account_domain_input_field() { ?>
-		<input
-			type="text"
-			id="webdam_account_domain"
-			name="webdam_settings[webdam_account_domain]"
-			value="<?php echo ! empty( $this->settings['webdam_account_domain'] ) ? esc_attr( $this->settings['webdam_account_domain'] ) : ''; ?>"
-			placeholder="yourdomain.webdamdb.com"/><?php
-	}
+		// Fetch our existing settings
+		$settings = get_option( 'webdam_settings' );
 
-	/**
-	 * Get the settings option array and print one of its values
-	 */
-	public function webdam_account_username_input_field() { ?>
-		<input
-			type="text"
-			id="webdam_account_username"
-			name="webdam_settings[webdam_account_username]"
-			value="<?php echo ! empty( $this->settings['webdam_account_username'] ) ? esc_attr( $this->settings['webdam_account_username'] ) : ''; ?>" /><?php
-	}
+		// Determine if we're authenticated or not
+		if ( \webdam_is_authenticated() ) {
+			$api_status_text = __( 'API Authenticated', 'webdam' );
+			$api_status_class = 'authenticated';
+		} ?>
+		
+		<div class="webdam-settings wrap <?php echo esc_attr( $api_status_class ); ?>">
+			<h2><?php echo esc_html_e( 'WebDAM Settings', 'webdam' ); ?></h2>
+			<form method="post" action="options.php"><?php
 
-	/**
-	 * Get the settings option array and print one of its values
-	 */
-	public function webdam_account_password_input_field() { ?>
-		<input
-			type="text"
-			id="webdam_account_password"
-			name="webdam_settings[webdam_account_password]"
-			value="<?php echo ! empty( $this->settings['webdam_account_password'] ) ? esc_attr( $this->settings['webdam_account_password'] ) : ''; ?>" /><?php
-	}
+				// This prints out all hidden setting fields
+				settings_fields( 'webdam_settings' ); ?>
 
-	/**
-	 * Get the settings option array and print one of its values
-	 */
-	public function api_client_id_input_field() { ?>
-		<input
-			type="text"
-			id="api_client_id"
-			name="webdam_settings[api_client_id]"
-			value="<?php echo ! empty( $this->settings['api_client_id'] ) ? esc_attr( $this->settings['api_client_id'] ) : ''; ?>" /><?php
-	}
+				<table class="form-table">
+					<tbody>
+						<tr>
+							<th scope="row">
+								<label for="webdam_account_domain"><?php esc_html_e( 'Domain', 'webdam' ); ?></label>
+							</th>
+							<td>
+								<input
+									type="text"
+									id="webdam_account_domain"
+									name="webdam_settings[webdam_account_domain]"
+									value="<?php echo ! empty( $settings['webdam_account_domain'] ) ? esc_attr( $settings['webdam_account_domain'] ) : ''; ?>"
+									placeholder="yourdomain.webdamdb.com">
+							</td>
+						</tr><tr id="api-status-row">
+							<th scope="row">
+								<label for="webdam_enable_api"><?php esc_html_e( 'API Status', 'webdam' ); ?></label>
+							</th><td>
+								<p class="api-authentication-status">
+									<span class="<?php echo esc_attr( $api_status_class ); ?>">
+										<?php echo esc_html( $api_status_text ); ?>
+									</span>
+								</p><?php
 
-	/**
-	 * Get the settings option array and print one of its values
-	 */
-	public function api_client_secret_input_field() { ?>
-		<input
-			type="text"
-			id="api_client_secret"
-			name="webdam_settings[api_client_secret]"
-			value="<?php echo ! empty( $this->settings['api_client_secret'] ) ? esc_attr( $this->settings['api_client_secret']) : ''; ?>" /><?php
+								// Display link to authenticate if needed
+								if ( \webdam_is_authenticated() ) {
+
+									// @todo button to manually refresh token?
+
+									// @todo button to test API?
+
+								} else {
+									// Once we have client_id/secret show the api auth_code link
+									if ( empty( $settings['api_client_secret'] ) || empty( $settings['api_client_id'] ) ) {
+
+										printf(
+											'<p>%s<p><p><a target="_blank" href="%s" title="%s">%s</a></p>',
+											esc_html__( 'Enter your WebDAM Client ID and Secret Keys below.', 'webdam' ),
+											esc_url( 'http://webdam.com/DAM-software/API/' ),
+											esc_attr__( 'Obtain your API keys', 'webdam' ),
+											esc_html__( 'Click here to obtain your keys.', 'webdam' )
+										);
+									} else {
+										// Display the authorization link
+										// this link takes user to webdam to login and authorize our API
+										printf(
+											'<p><a href="%s" title="%s" class="%s">%s</a></p>',
+											esc_url( \webdam_get_authorization_url() ),
+											esc_attr__( 'Authorize WebDAM', 'webdam' ),
+											esc_attr( 'authorization-url' ),
+											esc_html__( 'Click here to authorize API access to your WebDAM account.', 'webdam' )
+										);
+										// Display a notice for the user to enter their api keys
+									}
+								} ?>
+							</td>
+						</tr><tr id="api-client-id-row">
+							<th scope="row"><?php esc_html_e( 'API Client ID', 'webdam' ); ?></th>
+							<td>
+								<input
+									type="text"
+									id="api_client_id"
+									name="webdam_settings[api_client_id]"
+									value="<?php echo ! empty( $settings['api_client_id'] ) ? esc_attr( $settings['api_client_id'] ) : ''; ?>">
+							</td>
+						</tr><tr id="api-client-secret-row">
+							<th scope="row"><?php esc_html_e( 'API Client Secret', 'webdam' ); ?></th>
+							<td>
+								<input
+									type="text"
+									id="api_client_secret"
+									name="webdam_settings[api_client_secret]"
+									value="<?php echo ! empty( $settings['api_client_secret'] ) ? esc_attr( $settings['api_client_secret'] ) : ''; ?>">
+							</td>
+						</tr>
+					</tbody>
+				</table><?php
+
+				submit_button(); ?>
+
+			</form>
+		</div><?php
+
 	}
 
 	/**
@@ -284,14 +281,6 @@ class Admin {
 			$new_settings['webdam_account_domain'] = sanitize_text_field( $input['webdam_account_domain'] );
 		}
 
-		if( isset( $input['webdam_account_username'] ) ) {
-			$new_settings['webdam_account_username'] = sanitize_text_field( $input['webdam_account_username'] );
-		}
-
-		if( isset( $input['webdam_account_password'] ) ) {
-			$new_settings['webdam_account_password'] = sanitize_text_field( $input['webdam_account_password'] );
-		}
-		
 		if( isset( $input['api_client_id'] ) ) {
 			$new_settings['api_client_id'] = sanitize_text_field( $input['api_client_id'] );
 		}
