bd03a2af2a6167c717de8f88acda54bebfff77cc
James Mehorter <james.mehorter@pmc.com>
2016-03-28 17:29:40 -0400

    PMCVIP-831 - Debugging helper for QA

--- includes/class-admin-settings.php
+++ includes/class-admin-settings.php
@@ -164,8 +164,6 @@ class Admin {
 		$api = API::get_instance();
 		print_r($api);
 
-		phpinfo();
-
 		// Set some default items
 		$api_status_text = __( 'API NOT Authenticated', 'webdam' );
 		$api_status_class = 'not-authenticated';
