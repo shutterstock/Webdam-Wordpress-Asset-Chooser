aeb63facde3cb1ea81638a42dbee3bd5e678260e
James Mehorter <james.mehorter@pmc.com>
2016-05-19 12:50:00 -0400

    Only load the API class when API usage has been enabled via settings

--- webdam-asset-chooser.php
+++ webdam-asset-chooser.php
@@ -16,9 +16,17 @@ define( 'WEBDAM_PLUGIN_SLUG', 'webdam-asset-chooser' );
 define( 'WEBDAM_PLUGIN_URL', trailingslashit( plugins_url( '', __FILE__ ) ) );
 
 require_once __DIR__ . '/includes/class-core.php';
+
+$settings = get_option( 'webdam_settings' );
+
+// Only load up the API class if API usage
+// has been enabled in Settings > WebDAM
+if ( ! empty( $settings['enable_api'] ) ) {
+	require_once __DIR__ . '/includes/class-api.php';
+}
+
 require_once __DIR__ . '/includes/helpers.php';
 require_once __DIR__ . '/includes/class-admin-settings.php';
-require_once __DIR__ . '/includes/class-api.php';
 require_once __DIR__ . '/includes/class-asset-chooser.php';
 
 // EOF
